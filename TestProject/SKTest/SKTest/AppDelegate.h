f3dc294791168c0367b45705d99b98bdebbd4849
vitorio ciumac <ciumvac@vitorio-ciumacs-MacBook-Pro.local>
2015-12-24 12:32:12 +0200

    project added

---
+++ TestProject/SKTest/SKTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SKTest
+//
+//  Created by vitorio ciumac on 12/24/15.
+//  Copyright © 2015 vitorio ciumac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProject/SKTest/SKTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SKTest
+//
+//  Created by vitorio ciumac on 12/24/15.
+//  Copyright © 2015 vitorio ciumac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
